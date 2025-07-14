e11d4d250e5090c602734f80cba578333448cea6
Osvaldo Victor <123516423+Djosekispy@users.noreply.github.com>
2025-07-14 18:27:30 +0000

    Welcome page done

---
+++ app/(welcome)/initial.tsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+import animation from '@/assets/animations/driver.json';
+
+
+export default function Initial() {
+
+  return (
+    <View className='flex-1 items-center justify-center'>
+         <View className='items-center justify-center text-justify px-8'>
+                <Text style={{fontFamily : 'Poppins'}} className='text-center text-md text-bold'>Aprenda Tudo sobre condução em qualquer lugar e saia na frente em qualquer teste</Text>
+          </View>
+       <LottieView
+            source={animation} 
+            autoPlay
+            loop
+            style={styles.animation}
+          />
+          <View  className='items-center justify-center'>
+            <TouchableOpacity className='bg-[#383A44] px-8 py-4 rounded-full'>
+                <Text className='text-center font-bold text-lg text-white' style={{fontFamily : 'Poppins'}}>Começar</Text>
+            </TouchableOpacity>
+          </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  animation: {
+    width: 500,
+    height: 500,
+  }
+});
